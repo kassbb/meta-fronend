8d0db502d21a4213d8c8ba2ee97ac006d1263903
Kassambara <137460089+kassbb@users.noreply.github.com>
2024-12-18 13:25:15 +0000

    style

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,39 +1,41 @@
 import React from 'react';
-import logo from './assets/Logo .svg'
-
+import logo from './assets/Logo .svg';
+import './Footer.css'
 const Footer = () => {
-    return (<div>
-            <img src={logo} alt="logo"/>
+    return (
+        <footer>
+            <div>
+                <img src={logo} alt="logo" style={{ width: "120px", height: "auto" }} />
+            </div>
             <div>
                 <h4>Doormat navigation</h4>
                 <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Menu</a></li>
-                    <li><a href="">Reservation</a></li>
-                    <li><a href="">Order Online</a></li>
-                    <li><a href="">Login</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservation</a></li>
+                    <li><a href="#">Order Online</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
             </div>
             <div>
                 <h4>Contact</h4>
                 <ul>
-                    <li>adress</li>
+                    <li>Address</li>
                     <li>Phone number</li>
                     <li>Email</li>
                 </ul>
-
-
             </div>
             <div>
                 <h4>Social Media</h4>
                 <ul>
-                    <li>adress</li>
-                    <li>Phone number</li>
-                    <li>Email</li>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
                 </ul>
             </div>
-        </div>);
+        </footer>
+    );
 };
 
 export default Footer;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/Logo.svg';
-import './Header.css'; // Importer le fichier CSS
+import './Header.css';
 
 function Header() {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(prevState => !prevState);
+    };
+
+    // Note: Log dans le console pour le débogage n'est pas nécessaire en production
+    useEffect(() => {
+        console.log(isMenuOpen ? "Menu ouvert" : "Menu fermé");
+    }, [isMenuOpen]);
+
     return (
-        <header> {/* Utilisation de la balise header pour le sémantique */}
+        <header>
             <div>
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="menu-icon" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
             <nav>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Menu</a></li>
-                    <li><a href="">Reservation</a></li>
-                    <li><a href="">Order Online</a></li>
-                    <li><a href="">Login</a></li>
+                <ul className={isMenuOpen ? 'open' : ''}>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservation</a></li>
+                    <li><a href="#">Order Online</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
             </nav>
         </header>
